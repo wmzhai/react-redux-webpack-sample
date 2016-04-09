dc18ea4efc5c60dd821d6acceb8e17b318a76ee0
Weiming Zhai <wmzhai@gmail.com>
2016-04-09 14:01:00 +0800

    add first component

--- client/client.js
+++ client/client.js
@@ -1,1 +1,12 @@
+import React from 'react'; // 等价于 var React = require('react')
+import { render } from 'react-dom'; // 等价于 var render = require('react-dom').render
+import App from '../components/App'
+
+render(
+  // 定义Component
+  // DOM 元素上mount
+  <App />,
+  document.getElementById('app')
+)
+
 
--- components/App.js
+++ components/App.js
@@ -1,1 +1,11 @@
+import React, { Component } from 'react';
 
+class App extends Component {
+  render() {
+    return (
+      <div> 这是一个React应用 </div>
+    )
+  }
+}
+
+export default App;
