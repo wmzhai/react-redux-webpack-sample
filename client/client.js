000b6f4c2addbb6da8181f4a834f94604f989ca6
Weiming Zhai <wmzhai@gmail.com>
2016-04-12 15:13:15 +0800

    add action reducer and store

--- client/client.js
+++ client/client.js
@@ -1,11 +1,26 @@
-import React from 'react'; // 等价于 var React = require('react')
-import { render } from 'react-dom'; // 等价于 var render = require('react-dom').render
+import React from 'react' // 等价于 var React = require('react')
+import { render } from 'react-dom' // 等价于 var render = require('react-dom').render
 import App from '../components/App'
+import configureStore from '../redux/store'
+import { Provider } from 'react-redux'
+
+let initialState = {
+  todos: [{
+    id: 0,
+    completed: false,
+    text: 'Initial todo for demo purpose'
+  }]
+};
+
+let store = configureStore(initialState);
+
 
 render(
   // 定义Component
   // DOM 元素上mount
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('app')
 )
 
